ae1cf8d9bf0df5dbf5130c029459e140868942b4
Abdi <>
2022-11-16 14:31:29 +0700

    Created json schema maker web.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+if (isset($_FILES['file'])) {
+    try {
+        $file = $_FILES['file'];
+        $fileContent = file_get_contents($file['tmp_name']);
+
+        $instanceValue = json_decode($fileContent);
+        $schema = new \Swaggest\JsonSchema\Schema();
+        $f = new \Swaggest\JsonSchemaMaker\SchemaMaker($schema);
+        $f->options->upgradeIntToNumber = true; 
+        $f->addInstanceValue($instanceValue);
+
+        $schemaJson = json_encode(\Swaggest\JsonSchema\Schema::export($schema));
+        $schemaJsonInline = json_encode($schema);
+
+        header('Content-Type: application/json; charset=utf-8');
+
+        echo $schemaJsonInline;
+        } catch (\Throwable $th) {
+            var_dump($th->getMessage());
+        }
+}
+?>
