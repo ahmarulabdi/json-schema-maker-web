391d5670b90fea2f106b79742ccb50b568f7832a
Abdi <>
2022-11-27 20:49:22 +0700

    Add json schema maker for text input.

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
-
-if (isset($_FILES['file'])) {
-    try {
+try {
+    if (isset($_FILES['file'])) {
         $file = $_FILES['file'];
         $fileContent = file_get_contents($file['tmp_name']);
 
         $instanceValue = json_decode($fileContent);
-        $schema = new \Swaggest\JsonSchema\Schema();
-        $f = new \Swaggest\JsonSchemaMaker\SchemaMaker($schema);
-        $f->options->heuristicRequired = true;
-        $f->addInstanceValue($instanceValue);
+    }
 
-        $schemaJson = json_encode(\Swaggest\JsonSchema\Schema::export($schema));
-        $schemaJsonInline = json_encode($schema);
+    if (isset($_POST['json_data'])) {
+        $instanceValue = json_decode($_POST['json_data']);
+    }
 
-        header('Content-Type: application/json; charset=utf-8');
+    $schema = new \Swaggest\JsonSchema\Schema();
+    $f = new \Swaggest\JsonSchemaMaker\SchemaMaker($schema);
+    $f->options->heuristicRequired = true;
+    $f->addInstanceValue($instanceValue);
 
-        echo $schemaJsonInline;
-        } catch (\Throwable $th) {
-            var_dump($th->getMessage());
-        }
+    $schemaJson = json_encode(\Swaggest\JsonSchema\Schema::export($schema));
+    $schemaJsonInline = json_encode($schema);
+
+    header('Content-Type: application/json; charset=utf-8');
+
+    echo $schemaJsonInline;
+} catch (\Throwable $th) {
+    var_dump($th->getMessage());
 }
+
 ?>
